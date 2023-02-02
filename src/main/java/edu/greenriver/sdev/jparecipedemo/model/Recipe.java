65578d994a63bb1870b8c19f59f16e148ad5e36b
angelmonique <campos.angel@student.greenriver.edu>
2023-02-02 10:37:37 -0800

    jpa-recipe-demo configured database

---
+++ src/main/java/edu/greenriver/sdev/jparecipedemo/model/Recipe.java
@@ -0,0 +1,31 @@
+package edu.greenriver.sdev.jparecipedemo.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Recipe
+{
+    //an auto increment primary key (aka surrogate key)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private List<String> ingredients;
+    private double servings;
+    private int cookTime;
+    private boolean vegan;
+}
+
---
+++ src/test/java/edu/greenriver/sdev/jparecipedemo/JpaRecipeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.greenriver.sdev.jparecipedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaRecipeDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
